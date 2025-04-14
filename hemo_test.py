0386b59b9885035b0b1e1d219c2b639bf6ebe584
jesse <jwang6174@gmail.com>
2025-04-13 23:32:20 -0400

    Use R2 instead of R

--- hemo_test.py
+++ hemo_test.py
@@ -13,16 +13,16 @@ from scipy.stats import pearsonr, t
 from sklearn.metrics import mean_squared_error
 
 
-def get_pcc(x, y):
+def get_r2(x, y):
   """
   Args:
     x (ndarray): Test vector.
     y (ndarray): Real vector.
 
   Returns:
-    pcc_r (float): Pearson correlation coefficient.
-    pcc_ci95_lower (float): Lower limit of 95% CI.
-    pcc_ci95_upper (float): Upper limit of 95% CI.
+    r2 (float): Coefficient of determination.
+    r2_ci95_lower (float): Lower limit of 95% CI.
+    r2_ci95_upper (float): Upper limit of 95% CI.
   """
   result = pearsonr(x, y)
   pcc_r = result.statistic
@@ -30,9 +30,11 @@ def get_pcc(x, y):
   pcc_ci95 = result.confidence_interval(confidence_level=0.95)
   pcc_ci95_lower = pcc_ci95.low
   pcc_ci95_upper = pcc_ci95.high
-  return pcc_r[0], pcc_ci95_lower[0], pcc_ci95_upper[0]
+  r2 = pcc_r[0] ** 2
+  r2_ci95_lower = pcc_ci95_lower[0] ** 2
+  r2_ci95_upper = pcc_ci95_upper[0] ** 2
+  return r2, r2_ci95_lower, r2_ci95_upper
   
-
 
 def get_rmse(x, y):
   """
@@ -99,7 +101,9 @@ def test(model_name, data_name, data_fold):
   # Iterate through each data fold, concatenating all real and predicted vectors.
   for data_fold in data_folds:
     
-    model_best_path = os.path.join('models', f'{model_name}_{data_fold}', 'model_best.chk')
+    model_best_path = os.path.join('models', 
+                                  f'{model_name}_{data_fold}', 
+                                   'model_best.chk')
     checkpoint = torch.load(model_best_path, weights_only=False)
     model = CardiovascularPredictor()
     model.load_state_dict(checkpoint['model_state_dict'])
@@ -119,23 +123,33 @@ def test(model_name, data_name, data_fold):
     
     # Iterate through all test segments.
     for i, (acc, ecg, bmi, label) in enumerate(test_loader, start=1):
+
+      # Get raw predicted and actual RHC value.
       real_y = label.detach().numpy().squeeze(-1)
       pred_y = model(acc, ecg, bmi).detach().numpy()
+
+      # Reverse z-score normalization.
       real_y = reverse_z_score_norm(real_y, global_stats)
       pred_y = reverse_z_score_norm(pred_y, global_stats)
+
+      # Concatenate RHC values.
       all_real = real_y if all_real is None else np.concat((all_real, real_y))
       all_pred = pred_y if all_pred is None else np.concat((all_pred, pred_y))
+
+      # Calculate root mean squared error and pearson correlation coefficient.
       rmse, rmse_ci95_lower, rmse_ci95_upper = get_rmse(all_real, all_pred)
-      pcc, pcc_ci95_lower, pcc_ci95_upper = get_pcc(all_real, all_pred)
-      print(f'Fold = {data_fold}, Batch = {i}/{len(test_loader)}, RMSE = {rmse:.2f}, PCC = {pcc:.2f}') 
+      r2, r2_ci95_lower, r2_ci95_upper = get_r2(all_real, all_pred)
+
+      print(f'Fold = {data_fold}, Batch = {i}/{len(test_loader)}, '
+            f'RMSE = {rmse:.2f}, R2 = {r2:.2f}') 
   
   results = {
     'rmse': rmse,
     'rmse_ci95_lower': rmse_ci95_lower,
     'rmse_ci95_upper': rmse_ci95_upper,
-    'pcc': pcc,
-    'pcc_ci95_lower': pcc_ci95_lower,
-    'pcc_ci95_upper': pcc_ci95_upper
+    'r2': r2,
+    'r2_ci95_lower': r2_ci95_lower,
+    'r2_ci95_upper': r2_ci95_upper
   }
 
   for k, v in results.items():
